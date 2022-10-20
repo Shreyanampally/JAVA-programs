0cf7e6ada926f7e131f64691385418bf2cf30c03
ShreyaNampally <92994005+Shreyanampally@users.noreply.github.com>
2022-10-20 12:25:00 +0530

    Create GoBackN.java

---
+++ GoBackN.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++){
+		    System.out.println("Frame "+ i +" has been transmitted.");
+		}
+		System.out.println("Please enter the last Acknowledgement received.");
+		int k=sc.nextInt();
+		for(int i=k;i<n;i++){
+		    System.out.println("Frame "+ i +" has been transmitted.");
+		}
+	}
+}
